49732ec67f59dbbc0c6d58c02d707e49b6538830
Vterebenin <terebenin.work@gmail.com>
2020-07-26 17:35:55 +0300

    feat: добавил бэкенд для api на рельсах

--- recordstore-back/config/routes.rb
+++ recordstore-back/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :artists do
+        resources :records
+      end
+    end
+  end
+
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
 end
