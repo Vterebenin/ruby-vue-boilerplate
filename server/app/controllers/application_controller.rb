770d924393776fd663574c0458316ed552c1a63d
Vterebenin <terebenin.work@gmail.com>
2020-07-26 14:12:17 +0300

    feat: rails-guides getting-started finished

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :print_params
+
+  private
+    def print_params
+      puts "-----------------------------------"
+      puts params
+      puts "-----------------------------------"
+    end
 end
