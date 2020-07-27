f85ecabab108f7f4c5ef002276627a665f840f14
Vterebenin <terebenin.work@gmail.com>
2020-07-28 00:17:36 +0300

    ref: Header&Logo -> vuetify

---
+++ frontend/components/Header.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  v-app-bar(app color="white")
+    v-app-bar-nav-icon
+      logo
+    v-toolbar-title(to="/") RecordStore
+    v-spacer
+    v-toolbar-items
+      v-btn(text v-if="!signedIn" to="/") Sign in
+      v-btn(text v-if="!signedIn" to="/signup") Sign Up
+      v-btn(text v-if="signedIn" to="/records") Records
+      v-btn(text v-if="signedIn" to="/artists") Artists
+      v-btn(text v-if="signedIn" href="#" @click.prevent="signOut") Sign out
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  name: 'Header',
+  components: { Logo },
+  computed: {
+    signedIn () {
+      console.log(localStorage.getItem('signedIn'))
+      return localStorage.getItem('signedIn')
+    }
+  },
+  methods: {
+    setError (error, text) {
+      this.error = (error.response && error.response.data && error.response.data.error) || text
+    },
+    signOut () {
+      this.$secured.delete('/signin')
+        .then((response) => {
+          localStorage.removeItem('signedIn')
+          localStorage.removeItem('csrf')
+          this.$router.replace('/')
+        })
+        .catch(error => this.setError(error, 'Cannot sign out'))
+    }
+  }
+}
+</script>
---
+++ frontend/components/Logo.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  svg(color="black" viewBox="0 0 24 24" width="24" height="24")
+    title record vinyl
+    path(d="M23.938 10.773a11.915 11.915 0 0 0-2.333-5.944 12.118 12.118 0 0 0-1.12-1.314A11.962 11.962 0 0 0 12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12c0-.414-.021-.823-.062-1.227zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-5a1 1 0 1 0 0 2 1 1 0 0 0 0-2z")
+</template>
+
+<script>
+export default {
+  name: 'Logo'
+}
+</script>
